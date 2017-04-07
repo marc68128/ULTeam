f5b12a2a7e7991fd7ec60311e80960c57d59cb9a
Marc Unterseh <marc.unterseh@i-datech.fr>
2017-04-07 16:09:06 +0200

    file transfert

--- ULTeam/MainWindow.xaml.cs
+++ ULTeam/MainWindow.xaml.cs
@@ -1,18 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using ULTeam.Enums;
+using ULTeam.Utils;
 
 namespace ULTeam
 {
@@ -21,17 +17,21 @@ namespace ULTeam
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<DriveInfo> _drives;
+        private string _selectedDrive;
+        private Skydiver _selectedSkydiver;
+
         public MainWindow()
         {
             InitializeComponent();
+            _drives = DriveInfo.GetDrives().Where(d => d.DriveType == DriveType.Removable).ToList();
         }
 
-        public Skydiver SelectedSkydiver { get; set; }
         private void SelectSkydiver(object sender, MouseButtonEventArgs e)
         {
-            SelectedSkydiver = (Skydiver)Enum.Parse(typeof(Skydiver), (sender as ProgressBar).Name.Replace("ProgressBar", ""));
+            _selectedSkydiver = (Skydiver)Enum.Parse(typeof(Skydiver), (sender as ProgressBar).Name.Replace("ProgressBar", ""));
             RenaudBorder.Visibility = TomBorder.Visibility = MarcBorder.Visibility = Visibility.Collapsed;
-            switch (SelectedSkydiver)
+            switch (_selectedSkydiver)
             {
                 case Skydiver.Renaud:
                     RenaudBorder.Visibility = Visibility.Visible;
@@ -44,6 +44,47 @@ namespace ULTeam
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
+            }
+
+            ListenDrive();
+        }
+
+        private async void ListenDrive()
+        {
+            await Task.Run(() =>
+            {
+                while (true)
+                {
+                    var drives = DriveInfo.GetDrives().Where(d => d.DriveType == DriveType.Removable).ToList();
+                    if (drives.Count > _drives.Count)
+                    {
+                        var addedDrive = drives.Except(_drives).First();
+                        _drives = drives;
+                        DriveAdded(addedDrive);
+                        continue;
+                    }
+                    if (drives.Count < _drives.Count)
+                    {
+                        _drives = drives;
+                    }
+                }
+            });
+        }
+
+        private void DriveAdded(DriveInfo addedDrive)
+        {
+            var drivePath = addedDrive.RootDirectory.FullName;
+            List<string> videoPaths = new List<string>(); //TODO Find vidéos in gopro root folder
+
+            var extPath = $"/{DateTime.Today.Year}/{DateTime.Today.ToString("MMMM", CultureInfo.GetCultureInfo("fr-FR"))}/{DateTime.Today.Day}";
+            var jumpNumber = Directory.GetDirectories(extPath).Select(d => int.Parse(d.Replace("Saut", ""))).Max() + 1;
+            extPath = Path.Combine(extPath, "Saut" + jumpNumber, _selectedSkydiver + ".mp4");
+
+            for (var index = 0; index < videoPaths.Count; index++)
+            {
+                var videoPath = videoPaths[index];
+                CustomFileCopier fileCopier = new CustomFileCopier(videoPath, extPath);
+                extPath = Path.Combine(Path.GetDirectoryName(extPath), _selectedSkydiver + (index + 1) + ".mp4");
             }
         }
     }
---
+++ ULTeam/Utils/CustomFileCopier.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+
+namespace ULTeam.Utils
+{
+    public delegate void ProgressChangeDelegate(double persentage, ref bool cancel);
+    public delegate void Completedelegate();
+
+    public class CustomFileCopier
+    {
+        public CustomFileCopier(string source, string dest)
+        {
+            SourceFilePath = source;
+            DestFilePath = dest;
+
+            OnProgressChanged += delegate { };
+            OnComplete += delegate { };
+        }
+
+        public void Copy()
+        {
+            byte[] buffer = new byte[1024 * 1024]; // 1MB buffer
+
+            using (FileStream source = new FileStream(SourceFilePath, FileMode.Open, FileAccess.Read))
+            {
+                long fileLength = source.Length;
+                using (FileStream dest = new FileStream(DestFilePath, FileMode.CreateNew, FileAccess.Write))
+                {
+                    long totalBytes = 0;
+                    int currentBlockSize;
+
+                    while ((currentBlockSize = source.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        totalBytes += currentBlockSize;
+                        double persentage = totalBytes * 100.0 / fileLength;
+
+                        dest.Write(buffer, 0, currentBlockSize);
+
+                        var cancelFlag = false;
+                        OnProgressChanged?.Invoke(persentage, ref cancelFlag);
+
+                        if (cancelFlag)
+                        {
+                            // Delete dest file here
+                            break;
+                        }
+                    }
+                }
+            }
+
+            OnComplete?.Invoke(); 
+        }
+
+        public string SourceFilePath { get; set; }
+        public string DestFilePath { get; set; }
+
+        public event ProgressChangeDelegate OnProgressChanged;
+        public event Completedelegate OnComplete;
+    }
+}
